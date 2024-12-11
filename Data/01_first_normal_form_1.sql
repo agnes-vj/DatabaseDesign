2b333f9f1a340e2a420282fd493bfdbedfd1bec9
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-11 10:37:23 +0000

    move data into 1NF

--- Data/01_first_normal_form_1.sql
+++ Data/01_first_normal_form_1.sql
@@ -20,6 +20,7 @@ CREATE TABLE students
 );
 GO
 
+
 INSERT INTO students (student_id, student_name, courses, age)
 VALUES
     (10015, 'Joe', 'Python', 21),
@@ -31,4 +32,30 @@ VALUES
 GO
 
 SELECT * FROM students;
+
+DROP TABLE IF EXISTS studentsone;
+DROP TABLE IF EXISTS students1NF;
+CREATE TABLE students1NF
+( 
+    student_id INT,
+    student_name VARCHAR(30),
+    course VARCHAR(30),
+    age INT,
+    PRIMARY KEY (student_id, course)
+);
+
+INSERT INTO students1NF (student_id, student_name, course, age)
+VALUES
+    (10015, 'Joe', 'Python', 21),
+    (10028, 'Paul', 'Python', 18),
+    (10028, 'Paul', 'Javascript', 18),
+    (10037, 'Danika', 'C++', 20),
+    (10037, 'Danika', 'Java', 20),
+    (10051, 'Kyle', 'SQL', 18),
+    (10051, 'Kyle', 'Javascript', 18),
+    (10074, 'Alex', 'C++', 19),
+    (10088, 'Simon', 'Javascript', 20),
+    (10088, 'Simon', 'Java', 20);
+
 GO
+SELECT * FROM students1NF;
