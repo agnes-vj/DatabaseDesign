ca76d85df3ea17abe5075cad72ecd3238e22186a
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-12-11 11:59:21 +0000

    Add Employee table with 2NF

--- Data/06_second_normal_form_2.sql
+++ Data/06_second_normal_form_2.sql
@@ -33,6 +33,45 @@ INSERT INTO nc_restaurant_staff (employee_no, full_name, job_code, job_title, em
 	('NC0008', 'David Chang', 'J002', 'Chef', 'Part-time'),
 	('NC0009', 'Guy Fieri', 'J003', 'Bartender', 'Part-time');
 GO
+CREATE TABLE nc_restaurant_staff3NF
+( 
+    employee_no VARCHAR(20) PRIMARY KEY,            
+    full_name VARCHAR(25),             
+    job_code VARCHAR(15), 
+    employment VARCHAR(20)              
+);
+GO
+CREATE TABLE nc_restaurant_job
+( 
+    job_code VARCHAR(15) PRIMARY KEY,               
+    job_title VARCHAR(25) 
+);
+GO
+INSERT INTO nc_restaurant_staff3NF (employee_no, full_name, job_code, employment)
+	VALUES
+	('NC0001', 'Gordon Ramsay', 'J001', 'Full-time'),
+	('NC0002', 'Jamie Oliver', 'J004', 'Part-time'),
+	('NC0003', 'Rachael Ray', 'J002', 'Full-time'),
+	('NC0004', 'Wolfgang Puck', 'J003', 'Full-time'),
+	('NC0005', 'Giada De Laurentiis', 'J004', 'Full-time'),
+	('NC0006', 'Alan Ducasse', 'J004',  'Part-time'),
+	('NC0007', 'Anne Burrell', 'J002','Full-time'),
+	('NC0008', 'David Chang', 'J002', 'Part-time'),
+	('NC0009', 'Guy Fieri', 'J003', 'Part-time');
+GO
 
-SELECT * FROM nc_restaurant_staff;
+INSERT INTO nc_restaurant_job (job_code, job_title)
+	VALUES
+	('J001', 'Head Chef'),
+	('J002', 'Chef'),	
+	('J003', 'Bartender'),
+	('J004', 'Waiter');
+
+
+GO
+
+SELECT * FROM nc_restaurant_staff3NF;
+SELECT * FROM nc_restaurant_job;
+
+
 GO
